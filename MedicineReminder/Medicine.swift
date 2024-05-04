1008ada74e7c5d16bd30c5a9adf99d072b995a4e
sevvalkalkavan <sevvallkalkavannn@gmail.com>
2024-05-04 21:09:48 +0300

    SearcBar integrated.

--- MedicineReminder/Medicine.swift
+++ MedicineReminder/Medicine.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 class Medicine{
-    var id: Int?
+    //var id: Int?
     var image: String?
     var name: String?
     var dueDate: String?
     
-    init(id: Int? = nil, image: String? = nil, name: String? = nil, dueDate: String? = nil) {
-        self.id = id
+    init( image: String? = nil, name: String? = nil, dueDate: String? = nil) {
+        //self.id = id
         self.image = image
         self.name = name
         self.dueDate = dueDate
--- MedicineReminder/MedicineViewController.swift
+++ MedicineReminder/MedicineViewController.swift
@@ -12,33 +12,22 @@ import Kingfisher
 class MedicineViewController: UIViewController {
 
     @IBOutlet weak var medicineTableView: UITableView!
+    @IBOutlet weak var searchTF: UITextField!
     
-//    var medicineList = [Medicine]()
-    
-    var nameList = [String]()
-    var dateList = [String]()
-    var imageList = [String]()
+   var medicineList = [Medicine]()
+   var originalMedicineList = [Medicine]()
+//    var nameList = [String]()
+//    var dateList = [String]()
+//    var imageList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         medicineTableView.dataSource = self
         medicineTableView.delegate = self
-        
-//        let m1 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m1)
-//        let m2 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m2)
-//        let m3 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m3)
-//        let m4 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m4)
-//        let m5 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m5)
-//        let m6 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m6)
-//        let m7 = Medicine(id: 1, image: "pill", name: "Medicine Name" , dueDate: "22.05.2024")
-//        medicineList.append(m7)
+        searchTF.delegate = self
+
+
 
 //        self.navigationItem.title = "AppName"
 //        let appearance = UINavigationBarAppearance()
@@ -52,10 +41,17 @@ class MedicineViewController: UIViewController {
         fetchFirebase()
     }
     
+    
+    @IBAction func searchButton(_ sender: Any) {
+        searchTF.endEditing(true)
+        print(searchTF.text!)
+    }
+    
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.navigationItem.hidesBackButton = true
+        medicineTableView.reloadData()
     }
     
     
@@ -65,25 +61,27 @@ class MedicineViewController: UIViewController {
             if error != nil{
                 print(error?.localizedDescription)
             }else{
-                if snapshot?.isEmpty != true && snapshot != nil{
+                if let snapshot = snapshot, !snapshot.isEmpty {
+                                    self.originalMedicineList.removeAll(keepingCapacity: false)
+                                    self.medicineList.removeAll(keepingCapacity: false)
                     
-                    //bir değerin iki kere yüklenmesi gösterilmesini engeller
-                    self.dateList.removeAll(keepingCapacity: false)
-                    self.imageList.removeAll(keepingCapacity: false)
-                    self.nameList.removeAll(keepingCapacity: false)
-                    
-                    for document in snapshot!.documents{
+                    for document in snapshot.documents{
                         let documentId = document.documentID
                         print(documentId)
                         if let imageUrl = document.get("imageurl") as? String{
-                            self.imageList.append(imageUrl)
+                           // self.imageList.append(imageUrl)
+                            if let name = document.get("medicineName") as? String{
+                                //self.nameList.append(name)
+                                if let dueDate = document.get("dueDate") as? String{
+                                   // self.dateList.append(dueDate)
+                                    let medicine = Medicine(image: imageUrl, name: name, dueDate: dueDate)
+                                    self.medicineList.append(medicine)
+                                    self.originalMedicineList.append(medicine)
+                                }
+                            }
                         }
-                        if let name = document.get("medicineName") as? String{
-                            self.nameList.append(name)
-                        }
-                        if let dueDate = document.get("dueDate") as? String{
-                            self.dateList.append(dueDate)
-                        }
+                        
+                        
                         
                     }
                     self.medicineTableView.reloadData()
@@ -96,23 +94,51 @@ class MedicineViewController: UIViewController {
 
 }
 
-extension MedicineViewController: UITableViewDelegate, UITableViewDataSource{
+
+extension MedicineViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            let newText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+            searchMedicine(with: newText)
+            return true
+        }
+    
+    func searchMedicine(with searchText: String) {
+        guard !searchText.isEmpty else {
+            // If the search text is empty, reload the original list
+            medicineList = originalMedicineList
+            medicineTableView.reloadData()
+            return
+        }
+        
+        let filteredMedicines = originalMedicineList.filter { $0.name!.lowercased().contains(searchText.lowercased()) }
+        medicineList = filteredMedicines
+        medicineTableView.reloadData()
+    }
+        
+ 
+
+    
+}
+
+
+extension MedicineViewController: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        return medicineList.count
-        return nameList.count
+        return medicineList.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        let medicine = medicineList[indexPath.row]
           let cell = tableView.dequeueReusableCell(withIdentifier: "medicineCell", for: indexPath) as! MedicineTableViewCell
 //        cell.medicineImageView.image = UIImage(named: medicine.image!)
-        cell.nameMedicineLabel.text = nameList[indexPath.row]
-        cell.dueDateLabel.text = dateList[indexPath.row]
+        cell.nameMedicineLabel.text = medicineList[indexPath.row].name
+        cell.dueDateLabel.text = medicineList[indexPath.row].dueDate
         DispatchQueue.main.async {
-            cell.medicineImageView.kf.setImage(with: URL(string: self.imageList[indexPath.row]))
+            cell.medicineImageView.kf.setImage(with: URL(string: self.medicineList[indexPath.row].image!))
         }
        cell.backgroundColor = UIColor(named: "Color 1" )
        cell.cellBackground.layer.cornerRadius = 10.0
-     cell.cellBackground.backgroundColor = UIColor(named: "Color 2")
+       cell.cellBackground.backgroundColor = UIColor(named: "Color 2")
           return cell
         
         
@@ -120,6 +146,18 @@ extension MedicineViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toDetailVC", sender: self) 
+        medicineTableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+        if yOffset > 0 {
+            // Scrolling downwards, hide the navigation bar
+            navigationController?.setNavigationBarHidden(true, animated: true)
+        } else {
+            // Scrolling upwards, show the navigation bar
+            navigationController?.setNavigationBarHidden(false, animated: true)
+        }
     }
     
 }
